a474c3772e51e1593b793df9d95c1751d08cbdf9
xttpaopao <506626037@qq.com>
2025-06-06 17:33:10 +0800

    .gitignore

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import mujoco
 from mujoco import viewer
 
-model = mujoco.MjModel.from_xml_path("./urdf/Diana_7/urdf/mjmodel.xml")
+model = mujoco.MjModel.from_xml_path("./urdf/T170D/urdf/T170D.xml")
 
 data = mujoco.MjData(model)
 
 duration = 10.0  # seconds
 framerate = 60  # Hz
 mujoco.mj_resetData(model, data)
-data.ctrl = (0.0, 1.5, 0.0, 0.0, 0.0, 0.0, 0.0)
+# data.ctrl = (0.0, 1.5, 0.0, 0.0, 0.0, 0.0, 0.0)
 with viewer.launch_passive(model, data) as viewer:
     while viewer.is_running:
         # while data.time < duration:
